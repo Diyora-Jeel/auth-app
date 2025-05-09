264d1dc9211da3018253a76e2b2906c6a0bf29a4
Diyora-Jeel <jeeldiyora101@gmail.comm>
2025-05-09 15:30:01 +0530

    change db

--- src/dbConfig/dbConfig.js
+++ src/dbConfig/dbConfig.js
@@ -1,7 +1,17 @@
-import {PrismaClient} from "@prisma/client";
+// import {PrismaClient} from "@prisma/client";
 
-const prisma = new PrismaClient({
-    log:["query"]
-})
+// const prisma = new PrismaClient({
+//     log:["query"]
+// })
 
-export default prisma
+// export default prisma
+
+// dbConfig.js
+import { PrismaClient } from '@prisma/client';
+const globalForPrisma = global;
+
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
